a65dcaa494d1b72a8b57563b437d50e97e6d54d7
n0vember24 <minecraft11242006@gmail.com>
2023-05-06 14:48:05 +0500

    change django's default settings.py and create app

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    if os.path.exists('.env'):
+        load_dotenv('.env')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
